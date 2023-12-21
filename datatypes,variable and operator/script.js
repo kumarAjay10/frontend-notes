043623c899fb44ddc1d2b18ad51d8fc0c5892b39
kumarAjay10 <pukaleajay@gmail.com>
2023-12-21 12:04:54 +0530

    data types variables

---
+++ datatypes,variable and operator/script.js
@@ -0,0 +1,97 @@
+/* using the console 
+ uses REPL - read-evaluate-print-loop*/
+ /* variables 
+ variable : variables are container for data */
+//   variables rules //
+/* variables name are case sensitive : "a" & "A" are differnet
+ 2. only letter, underscore(-) and $ is allowed and (not even space)
+ 3. reserved words cannot be variables names.
+ /* Data types
+ 1. primitive datatypes 
+ 2. non primitive datatypes 
+ 
+
+ 1. primitive datatype :
+ 1. number 
+ example: */
+//  let num = 23; 
+//  console.log(num);
+//  console.log(typeof num);
+
+// 2. string we can use single appestophy
+//   let str = "ajay";
+//   console.log(str);
+//   console.log(typeof str);
+
+
+// boolean: is used to print true or false 
+ 
+// let num1 = 20;
+// let num2 = 15;
+// console.log(num1 < num2);
+
+
+// undefined: if dint assign a value to a variable it gives a undefined 
+
+// let a;
+// console.log(a);
+
+// null: null value indicates the absence of a value 
+
+// let a = null; 
+// console.log(a);
+
+
+// nan : not a number 
+
+// console.log(0/0);
+// console.log(NaN + 1);
+// console.log(NaN - 1);
+// console.log(NaN * 1);
+
+
+// operator in precendence : this is the order of solving a expression
+
+// ()
+// **
+// *,/,% here we go from left to right
+// +,-
+
+
+// console.log((5+2) / 7 + 1 * 2)  // 3
+// console.log(4+1*6/2);  // 7
+
+// operators //
+// 1. arthmetic operators 
+
+
+// 1. addition 
+// console.log(10 + 20);
+
+// 2. subtraction 
+// console.log(10 - 20);
+
+// 3. multiplacation
+// console.log(10 * 20);
+
+// 4. division 
+// console.log(10 / 20); // gives the qutoient value 
+
+// 5. modulo
+// console.log(20 % 10); // gives the remainder value
+
+
+// uranary operator 
+let age = 23;
+// age = age + 1;
+// age+= 1;
+// age++; post
+// ++age; pre
+// console.log(age);
+
+// practise question //
+// let num = 5;
+// let newNum = num++;
+// newNum = ++num;
+// console.log(newNum);// 7
+
