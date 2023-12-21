40ded8468d21de4e109965430b47960e7d3a4a08
kumarAjay10 <pukaleajay@gmail.com>
2023-12-21 22:49:48 +0530

    strings

---
+++ Strings/script.js
@@ -0,0 +1,53 @@
+// trim method : trim method remove whitespace from both the ends and return new one 
+
+// let msg = "     apna college      ";
+// console.log(msg.trim());
+
+
+// strings are immutable we cannot change the string 
+
+// uppercase method and lowercase method 
+
+// let msg = 'Apna College';
+// console.log(msg.toUpperCase());
+
+// let msg = 'Apna College';
+// console.log(msg.toLowerCase());
+
+// string method with arguments 
+
+// let str = "ilovecoding";
+// console.log(str.indexOf("l"));  // 1
+
+
+ //let str = "ilovecoding";
+// console.log(str.indexOf("love")); // 1
+
+// let str = "ilovecoding";
+//  console.log(str.indexOf("x"));  // -1
+
+// method chanining : using one method after another .. from left to right
+// let str = "      college      ";
+// console.log(str.trim().toUpperCase());
+
+
+// slice method //
+// return a part of original string to a new string
+  
+// slice(startindex, deleteindex);
+
+// let str = "ilovecoding";
+// console.log(str.slice(5,11));
+// console.log(str.slice(5,str.length));
+// console.log(str.slice(-6));
+
+
+// replace method //
+// let str = "ilovecoding";
+// console.log(str.replace("love","hate"));
+//  console.log(str.replaceAll("i","o"));
+
+// repeat method //
+// let str = "mango";
+// console.log(str.repeat(3));
+
