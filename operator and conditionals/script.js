0890e7e34b73bd9b6a1ae0aa6f943c2c14efb462
kumarAjay10 <pukaleajay@gmail.com>
2023-12-21 22:15:57 +0530

    conditionals

---
+++ operator and conditionals/script.js
@@ -0,0 +1,107 @@
+// template literals(string interpolation) : they are used to add embedded expression in a string 
+// let pencilPrice = 10;
+// let erasorPrice = 5;
+// console.log("the total price is :" + (pencilPrice + erasorPrice));
+// console.log(`the total price is ${pencilPrice + erasorPrice} `);
+
+
+// comparsion operator
+// >, <, >=, <=, ==, ===
+
+// comparsion operator for non-numbers
+
+ 
+//link of uncode;- https://symbl.cc/en/unicode/table/
+
+// console.log('a' > 'A'); // true  
+//  console.log('*' < '&');
+
+/* conditional statement 
+  1. if statement 
+   2.if else statement 
+   3. else if else statement (nested else if)
+   4. switch satement */
+ 
+//    if(5<20){
+//       console.log("5 is greater than 2");
+//    }
+
+
+//  if(5>20){
+//            console.log("5 is greater than 2");
+//    }else{
+//      console.log("ajay");
+//    }
+
+
+// let age = 18;
+// if(age>65){
+//     console.log("you are a senior citizen");
+// }
+// else if(age>18){
+//     console.log("you are adult");
+// }else{
+//     console.log("you are a kid");
+// }
+
+
+// switch statment 
+// let color = "red";
+// switch(color){
+//     case "red":
+//         console.log("stop");
+//         break;
+//         case "yellow":
+//             console.log("slow down");
+//             break;
+//             case "green":
+//                 console.log("green");
+// }
+
+
+// practice question 
+// let num = 50;
+// if( num%10 == 0 ){
+//       console.log("good");
+// }else{
+//     console.log("bad");
+// }
+
+// 2 question 
+//   let userName = prompt("enter your name: ");
+//   let age = prompt("enter your age: ");
+//   let output = alert(`${userName}  is ${age} years old`);
+//   console.log(output);
+
+// 3.question
+
+// let str = "golden string";
+// if(str[0]==="a" && str.length > 5){
+//     console.log("golden");
+// }else{
+//     console.log("not");
+// }
+
+
+// 4.question
+// to find the largest of 3 numbers
+// let a = 45;
+// let b = 34;
+// let c = 59;
+// if(a>b && a>c){
+//     console.log("A");
+// }else if(b>a && b>c){
+//     console.log("B");
+// }else{
+//     console.log("C");
+// }
+ 
+
+
+// let num1 = 2;
+// let num2 = 47852;
+// if(num1%10==2 && num2%10==2){
+//     console.log("A");
+// }else{
+//     console.log("B");
+// }
